85680cae0a3f3f3f911b73fd761ae0ee7ba0831c
Jongho Jeon <jongho.jeon@riiid.co>
2020-01-12 02:14:10 +0900

    :sparkles: Added gradle 6.0.1

--- init.sh
+++ init.sh
@@ -2,3 +2,10 @@
 git config --global user.email "jongho.jeon@riiid.co"
 git config --global user.name "Jongho Jeon"
 
+# gradle 6.0.1
+sudo add-apt-repository ppa:cwchien/gradle
+
+PKGS="gradle \
+    "
+sudo apt -y install $PKGS
+
