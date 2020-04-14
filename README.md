2a3a5a0bff517db54bc15653e4f4f0c9926cfe99
hojongs <hojong.jjh@gmail.com>
2020-04-14 15:00:04 +0100

    Enhanced init.sh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+### Mac or Linux
 crontab $DOTFILES/cronjobs.txt
--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
-DOTFILES_ROOT=~/dotfiles
+DOTFILES_ROOT=$(dirname $0)
 
-ln -s $DOTFILES_ROOT/.ideavimrc   ~/.ideavimrc
-ln -s $DOTFILES_ROOT/.vim_runtime ~/.vim_runtime
-ln -s $DOTFILES_ROOT/.vimrc       ~/.vimrc
-ln -s $DOTFILES_ROOT/.zshrc       ~/.zshrc
+ln -s      $DOTFILES_ROOT/.ideavimrc   ~/.ideavimrc
+echo ln -s $DOTFILES_ROOT/.ideavimrc   ~/.ideavimrc
+ln -s      $DOTFILES_ROOT/.vim_runtime ~/.vim_runtime
+echo ln -s $DOTFILES_ROOT/.vim_runtime ~/.vim_runtime
+ln -s      $DOTFILES_ROOT/.vimrc       ~/.vimrc
+echo ln -s $DOTFILES_ROOT/.vimrc       ~/.vimrc
+ln -s      $DOTFILES_ROOT/.zshrc       ~/.zshrc
+echo ln -s $DOTFILES_ROOT/.zshrc       ~/.zshrc
 
